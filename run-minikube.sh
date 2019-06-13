309fdee137cc9f2656a0792a10dd0b3fd6544b7e
Pimin Konstantin Kefaloukos <pimin.konstantin.kefaloukos@maersk.com>
2019-06-13 13:11:40 +0200

    added sleep to run script

--- run-minikube.sh
+++ run-minikube.sh
@@ -4,5 +4,8 @@ echo 'Remember to run: minikube start'
 kubectl run flask-walkthrough --image=docker.io/skipperkongen/flask-walkthrough:latest --port=80
 kubectl expose deployment flask-walkthrough --type=LoadBalancer --port=80
 kubectl get svc
+echo 'Sleeping 10 seconds to let containers come online...'
+sleep 10
+kubectl get svc
 curl $(minikube service flask-walkthrough --url)
 kubectl delete deployment,service flask-walkthrough
